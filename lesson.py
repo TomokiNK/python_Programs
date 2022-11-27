283311bd28dec9d49b8e69dc52a9a6c31b822cff
TomokiNK <tomoki19890110@gamil.com>
2022-11-27 18:51:15 +0900

    2022/11/27 python basic 1

--- lesson.py
+++ lesson.py
@@ -1,5 +1,72 @@
-num = 1
-name = '1'
-new_num = int(name)
-print(name, type(name))
-print(new_num, type(new_num))
+import math
+
+# ############練習 変数############
+# num = 1
+# name = '1'
+# new_num = int(name)
+# print(name, type(name))
+# print(new_num, type(new_num))
+
+# ############練習 出力(print)############
+# print('Hi', 'Mike', sep=',', end='.\n')
+# print(2 + 2)
+# result = math.sqrt(25)
+# print(result)
+
+# y = math.log2(10)
+# print(y)
+
+# print(help(math))
+
+# s = 'test'
+# print(s)
+# print('Hello')
+# print("I don't know")
+# print('say "I don\'t know"')
+# print("say \"I don't know\"")
+# print('Hello.\nHow are you?')
+# print('C:\name\name')
+# print(r'C:\name\name')
+#
+# print("####################")
+# print("""\
+# line1
+# line2
+# line3\
+# """)
+# print("####################")
+#
+# print('Hi.' * 3 + 'Mike')
+# print('py''thon')
+# prefix = 'py'
+# print(prefix + 'thon')
+#
+# word = 'python'
+# print(word[0])
+# print(word[1])
+# print(word[-1])
+# print(word[0:2])
+# print(word[:2])
+# print(word[2:6])
+# print(word[2:])
+# word = 'j' + word[1:]
+# print(word)
+# print(word[:])
+# n = len(word)
+# print(n)
+
+# s = 'My name is Mike. Hi Mike.'
+# print(s)
+# is_start = s.startswith('My')
+# print(is_start)
+#
+# print("#############################")
+# print(s.find('Mike'))
+# print(s.rfind('Mike'))
+# print(s.count('Mike'))
+# print(s.capitalize())
+# print(s.title())
+# print(s.upper())
+# print(s.lower())
+# print(s.replace('Mike', 'Nancy'))
+
