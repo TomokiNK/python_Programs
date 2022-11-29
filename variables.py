7d0635ded457d5cfd8b0b3f4d4c6a7305e4f26e8
TomokiNK <tomoki19890110@gamil.com>
2022-11-30 08:20:37 +0900

    2022/11/30 python variable 2

--- variables.py
+++ variables.py
@@ -8,3 +8,44 @@ print(l[2:])
 print(l[::2])
 print(l[::-1])
 
+s = ['a', 'b', 'c', 'd', 'e', 'f']
+print(s[0])
+s[0] = 'X'
+print(s[0])
+print(s[2:5])
+s[2:5] = ['B', 'C', 'D']
+print(s[2:5])
+
+s = []
+print(s)
+
+n = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+print(n)
+# リスト最後に値追加
+n.append(100)
+print(n)
+# リスト指定位置に値追加
+n.insert(0, 200)
+print(n)
+
+# リスト値の取り出し
+n.pop()
+print(n)
+n.pop(0)
+print(n)
+
+# リスト値の削除
+# n.remove(2)
+# print(n)
+
+del n[0]
+print(n)
+
+# リストの結合
+a = ['1', '2', '3']
+b = ['4', '5', '6']
+# s = a + b
+# print(s)
+# a += b
+a.extend(b)
+print(a)
